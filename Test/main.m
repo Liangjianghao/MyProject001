0e7262fac16a1057d0b9aa4ab8e012c19024754f
ljh <ljh@ljhdeMacBook-Pro.local>
2014-10-16 15:28:20 +0800

    Initial Commit

---
+++ Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test
+//
+//  Created by ljh on 14-10-16.
+//  Copyright (c) 2014年 ljh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
